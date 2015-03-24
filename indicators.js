bc144a1def9d71c4507bcc840a99213dc935201b
Christopher P.K Morris <chris_pkm@hotmail.co.uk>
2015-03-24 16:56:05 +0000

    Roilan from js irc indented my indicators script file to show how important code readability is

--- indicators.js
+++ indicators.js
@@ -1,52 +1,77 @@
+/*
+
+you should really get that sorted first because it's
+hard to understand/read
+
+I don't mind helping you sort out your JS a bit but
+have to take care of that first
+
+2 initIndicators functions? checking now.
+
+
+< slurp
+
+
+enter working? any luck?
+wow typing on the same line, causes abit of lag i think.
+it is quite laggy on my side, think there's 3 of us in this session
+my pc is crap aswell though so could have something to do with that.
+*/
 
 
 
-var initIndicators = function(){ 
 
-var amount = setInterval(function(){indicators()},600);
+var initIndicators = function(){
+	var amount = setInterval(function() {
+			indicators();
+		}, 600);
 
-function indicators(){
+	function indicators() {
 
-var fire = setInterval(function(){incro()},100);
+		var fire = setInterval(function() {
+			incro();
+		}, 100);
 
-var collect = []; //global scope, closed functions?
+		var collect = []; //global scope, closed functions?
 
-function incro(){
-var inc = 0; 
+		function incro() {
+			var inc = 0;
 
+			inc++;
 
-//while(inc<10){
-inc++; 
-//document.getElementById("count").innerHTML=collect.length; 
-collect.push(inc);
+			//document.getElementById("count").innerHTML=collect.length;
+			collect.push(inc);
 
-var spawn = document.createElement("div"); //.setAttribute("id","imageCube");
-spawn.style.width="10px"; 
-spawn.style.height="10px"; 
-spawn.style.cssFloat="left"; 
-spawn.style.backgroundColor="#81d4fa"; 
-spawn.style.margin="10px"; 
-spawn.style.borderRadius="5px";
-document.getElementById("indicators").appendChild(spawn);
+			var spawn = document.createElement("div"); //.setAttribute("id","imageCube");
 
-if(collect.length===4){
-clearInterval(fire);
-var dad = document.getElementById("indicators");
-while(dad.firstChild){ //keeps going for the length of the child divs that we're created. 
-    dad.removeChild(dad.firstChild);
-	collect.length = 0; 
-	inc = 0; 
-}
+			spawn.style.width="10px";
+			spawn.style.height="10px";
+			spawn.style.cssFloat="left";
+			spawn.style.backgroundColor="#81d4fa";
+			spawn.style.margin="10px";
+			spawn.style.borderRadius="5px";
 
-}
+			document.getElementById("indicators").appendChild(spawn);
 
-}; 
+			if(collect.length===4) {
+				clearInterval(fire);
 
-}; //end of indicators function
+				var dad = document.getElementById("indicators");
 
-}; 
+				//keeps going for the length of the child divs that we're created.
+				while(dad.firstChild) {
+				  dad.removeChild(dad.firstChild);
+					collect.length = 0;
+					inc = 0;
+				}
 
-var killIndicators = function(){ //change scope for the function to work. 
-	clearInterval(amount); 
-	clearInterval(fire); 
-}
+			}
+		};
+
+	}; //end of indicators function
+}; //< end of initIndicators...
+
+var killIndicators = function() { //change scope for the function to work. <here is what needs to be changed.
+	clearInterval(amount);
+	clearInterval(fire);
+};
